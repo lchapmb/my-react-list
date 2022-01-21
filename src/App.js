031e2a83dfd3d62dc0bf23471cae32f06cd4e620
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-01-21 14:35:33 +0000

    creates bork component and imports to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TodoList from "./components/todoList";
+import Bork from "./components/bork";
 
 function App() {
   return (
     <div className="App">
       <h1>Todo List</h1>
       <TodoList />
+      <Bork />
     </div>
   );
 }
